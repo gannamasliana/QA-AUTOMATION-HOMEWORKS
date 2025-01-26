6917607365b834a3db95bb9561bf097c6b39b44c
Ganna  Masliana <AMAslyanaya@gmail.com>
2025-01-26 22:16:06 +0100

    added Hello

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function sayHello(name, surname) {
     // some comment
-    console.log(`${name} ${surname}`);
+    console.log(`Hello ${name} ${surname}`);
 }
 //end function
 sayHello('Robot', 'Dreams');
